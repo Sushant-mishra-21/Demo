5c44f091faa8b80cc0dd1abc76116f4c3f79724f
Sushant-mishra-21 <sushant.mishra@globallogic.com>
2023-04-04 10:32:01 +0530

    check Commit

--- callback.js
+++ callback.js
@@ -1,5 +1,12 @@
-function getData(){
-  return new Promise (function (resolve,reject){
+  function hr(){
+    return new Promise (function (resolve,reject){
+      if (tech=='java'){
+        reject();
+      }
+      else if(tech=='React'){
+        resolve();
+      }
+    },3000)
+  }
 
-  })
-}
+  hr().then(function(){},function(){});
